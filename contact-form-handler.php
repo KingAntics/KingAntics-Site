5810aa46823b7f0b4077a5c9d3f9e96de3b4fa32
KingAntics <96980020+KingAntics@users.noreply.github.com>
2022-06-15 04:09:05 -0700

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,27 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+
+    $email_from = 'business@kingantics.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+
+
+    $to = "business@kingantics.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,email_subject,$email_body);
+
+    header("Location: index.html");
+
+
+?>
